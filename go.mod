2ec5157775c82032aaa35b2cee6e91923cf59a4e
Fiko Redha <fiko469@gmail.com>
2023-08-08 20:06:46 +0700

    update SayHello function parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/FRFebi/basic-modules
+module github.com/FRFebi/basic-modules/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -2,8 +2,8 @@ package go_say_hello
 
 import "fmt"
 
-func SayHello()string{
-	return "Hello Fiko"
+func SayHello(name string)string{
+	return "Hello "+name
 }
 
 func Test(){
